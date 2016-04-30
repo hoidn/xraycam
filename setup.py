f1fa0a555fda34eeb7d797440a6bd4a1063457c0
Oliver Hoidn <ohoidn@gmail.com>
2016-04-29 20:46:35 -0400

    Added dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name = 'xraycam',
     packages = find_packages('.'),
     package_dir = {'xraycam': 'xraycam'},
     package_data = {'xraycam': ['data/*']},
-    install_requires = ['paramiko'],
+    install_requires = ['paramiko', 'numpy', 'matplotlib'],
     zip_safe = False)
