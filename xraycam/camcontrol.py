f0ba5125a39e182fe1acfc3dbe47dc0dd4afb43f
Oliver Hoidn <ohoidn@gmail.com>
2016-05-04 22:06:47 -0400

    revertin mistaken change

--- xraycam/camcontrol.py
+++ xraycam/camcontrol.py
@@ -2,16 +2,19 @@ from paramiko import SSHClient
 import paramiko
 import numpy as np
 import time
-import detconfig
+from . import detconfig
 import argparse
 import matplotlib.pyplot as plt
 import pkg_resources
 import os
+import copy
+import pdb
+from functools import reduce
 
 PKG_NAME = __name__.split('.')[0]
 
 def resource_f(fpath):
-    from StringIO import StringIO
+    from io import StringIO
     return StringIO(pkg_resources.resource_string(PKG_NAME, fpath))
 
 def resource_path(fpath):
@@ -26,42 +29,57 @@ def adc_to_eV(adc_values):
     
 
 class DataRun:
-    def __init__(self, run_prefix = 'data/', run_name = 'exposure.' + str(time.time()),\
-            numExposures = 40, threshold = 15, window_min = 0, window_max = 255,\
+    def __init__(self, run_prefix = 'data/' + 'exposure.' + str(time.time()),\
+            run = False, numExposures = 40, threshold = 15, window_min = 0, window_max = 255,\
             gain = '0x7f', filter_sum = True):
         """
-        Runs a series of exposures on the camera.
+        Instantiate a DataRun and optionally run an exposure sequence
+        on the BBB.
+
+        run_prefix : str
+            Directory containing data files (relative to detconfig.base_path on the
+            BBB; relative to run directory locally)
+        reload : bool
+            If true, run an exposure sequence. (If false, we assume that output files
+            with the name prefix: run_prefix + run_name already exist on the BBB). 
+        TODO: etc..
         """
-        exposure_cmd = 'time sudo ./main_mt9m001 %d -o ' % threshold + run_prefix\
-            + run_name + ' -n ' + str(numExposures) + ' -g ' + gain +\
-            ' -r %d %d' % (window_min, window_max)
         self.gain = gain
         self.threshold = threshold
         self.numExposures = numExposures
         self.filter_sum = filter_sum
-        self.path = run_prefix + run_name
+        self.prefix = run_prefix
         self.filter_sum = filter_sum
-        if filter_sum:
-            exposure_cmd += ' -p'
 
-        keypath = resource_path('data/id_rsa.pub')
-        #keypath = "/home/oliver/.ssh/id_rsa.pub"
+        if run:
+            exposure_cmd = 'time sudo ./main_mt9m001 %d -o ' % threshold + run_prefix\
+                + ' -n ' + str(numExposures) + ' -g ' + gain +\
+                ' -r %d %d' % (window_min, window_max)
+            if filter_sum:
+                exposure_cmd += ' -p'
 
-        ssh = SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(detconfig.BBB_IP, 22, 'debian', key_filename=keypath)
+            #keypath = resource_path('data/id_rsa.pub')
 
-        #take an exposure
-        (sshin2, sshout2, ssherr2) = ssh.exec_command('cd' + ' ' + detconfig.base_path\
-            + '; ' + exposure_cmd)
-        print sshout2.read()
-        ssh.close()
+            ssh = SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(detconfig.BBB_IP, 22, 'debian', password = 'bbb')
+
+            #take an exposure
+            (sshin2, sshout2, ssherr2) = ssh.exec_command('cd' + ' ' + detconfig.base_path\
+                + '; ' + exposure_cmd)
+            print(sshout2.read())
+            ssh.close()
+
+        self.frame = Frame(array = self.get_array())
 
     def get_frame(self):
+        return self.frame
+
+    def get_array(self):
         """
         Get the sum of exposures for a dataset prefix as a numpy array
         """
-        name= self.path + 'sum.dat'
+        name= self.prefix + 'sum.dat'
         if not os.path.isfile(name):
             os.system('rsync -avz debian@' + detconfig.BBB_IP + ':' +\
                 detconfig.base_path + name + ' ' + name)
@@ -70,18 +88,18 @@ class DataRun:
     def get_histograms(self):
         def plot_one(name):
             if not os.path.isfile(name):# or args.reload:
-                get_ipython().system(u'scp -r debian@' + detconfig.BBB_IP +\
+                get_ipython().system('scp -r debian@' + detconfig.BBB_IP +\
                     ':' + detconfig.base_path + '/' + name + ' ' + name)
             return np.fromfile(name, dtype = 'uint32')
-        pixels = plot_one(self.path + 'pixels.dat')
-        singles = plot_one(self.path + 'singles.dat')
+        pixels = plot_one(self.prefix + 'pixels.dat')
+        singles = plot_one(self.prefix + 'singles.dat')
         return pixels, singles
 
-    def plot_histograms(self, pixels_plot = True, singles_plot = True, show = False,
+    def plot_histograms(self, pixels_plot = True, singles_plot = True, show = True,
             block = False,  calibrate = True, **kwargs):
         pixels, singles = self.get_histograms()
         #plt.ylim((0, np.max(pixels[15:])))
-        x = np.array(range(len(pixels)))
+        x = np.array(list(range(len(pixels))))
         if calibrate:
             plt.xlabel('Energy (eV)')
             x = adc_to_eV(x)
@@ -94,16 +112,85 @@ class DataRun:
         if show:
             plt.show(block = block)
 
-    def show(self):
-        plt.show()
+    def plot_lineout(self, show = False, filter = False, smooth = 0, **kwargs):
+        if filter:
+            lineout = self.frame.filter(**kwargs).lineout(smooth = smooth)
+        else:
+            lineout = self.frame.lineout(smooth = smooth)
+        plt.plot(lineout)
+        if show:
+            plt.show()
+        return lineout
+
+    def filter_frame(self, **kwargs):
+        self.frame = self.frame.filter(**kwargs)
+
+    def show(self, **kwargs):
+        self.frame.show(**kwargs)
+
+class RunSet:
+    """
+    Class containing a collection of DataRun instances.
+    """
+    def __init__(self, prefixes = None, number_runs = 0, run_prefix = None, **kwargs):
+        """
+        prefixes : list of str
+            A list of dataset prefixes.
+        number_runs : int
+        run_prefix : str
+        
+        Call using either a list of run prefixes
+        OR number_runs and run_prefix. 
+        """
+        if prefixes is None:
+            if run_prefix is None:
+                prefixes = [str(time.time()) for _ in range(number_runs)]
+            else:
+                prefixes = [base_name + '_%d' % i for i in range(number_runs)]
+        self.dataruns = [DataRun(run_prefix = prefix, **kwargs) for prefix in prefixes]
+
+    def insert(self,  datarun = None, *args, **kwargs):
+        """
+        datarun : DataRun
+
+        If datarun is provided, insert is into this RunSet. Otherwise pass
+        args and kwargs to the constructor for DataRun and insert the resulting
+        object into this RunSet.
+        """
+        if datarun is None:
+            datarun = DataRun(*args, **kwargs)
+        self.dataruns.append(datarun)
+
+    def get_dataruns(self):
+        return self.dataruns
+
+    def plot_lineouts(self, *args, show = True, **kwargs):
+        def one_lineout(datarun):
+            return datarun.plot_lineout(*args, show = False, **kwargs)
+        lineouts = list(map(one_lineout, self.dataruns))
+        if show:
+            plt.show()
+        return lineouts
+
+    def filter_reduce_frames(self, **kwargs):
+        """
+        Do artifact filtering for frames of all component data runs and return
+        merge all the resulting Frames into a new Frame instance.
+
+        kwargs are passed through to DataRun.filter
+        """
+        import operator
+        datarun_arrays = [dr.get_frame().get_data() for dr in self.dataruns]
+        frames = list(map(Frame, datarun_arrays))
+        def framefilter(frame):
+            return frame.filter(**kwargs)
+        return reduce(operator.add, list(map(framefilter, frames)))
 
 class Frame:
-    def __init__(self, datarun = None, frame = None):
+    def __init__(self, array = None, numExposures = 0):
         """Construct using either a DataRun instance, or another Frame."""
-        if datarun is not None:
-            self.data = datarun.get_frame()
-        elif frame is not None:
-            self.data = frame.data
+        self.data = array
+        self.numExposures = numExposures
 
     def show(self, **kwargs):
         """Show the frame. Kwargs are passed through to plt.imshow."""
@@ -114,13 +201,26 @@ class Frame:
         return self.data
 
     def __add__(self, other):
-        new = Frame(frame = self)
+        new = Frame(array = self.data)
         new.data = new.data + other.data
+        new.numExposures = self.numExposures + other.numExposures
         return new
         
+    def filter(self, threshold_min = 10, threshold_max = 150):
+        """
+        Returns a new Frame with artifacts filtered out.
+        """
+        new = copy.deepcopy(self)
+        data = new.data
+        lineout = np.sum(data, axis = 1)
+        mean, std = np.mean(lineout), np.std(lineout)
+        row_outliers = (np.abs(lineout - mean)/std > 2)
+        pixel_outliers = (data < threshold_min) | (data > threshold_max)
+        data[row_outliers[:, np.newaxis] | pixel_outliers] = 0.
+        return new
 
+    def lineout(self, smooth = 0):
+        from scipy.ndimage.filters import gaussian_filter as gf
+        return gf(np.sum(self.data, axis = 0), smooth)
 
-#def runsequence(exposures_per_run = 40, n_runs = 2, **kwargs):
-#    frames = [autorun_and_get_prefix(numExposures = exposures_per_run, **kwargs) for _ in range(n_runs)]
-#    return frames
 
