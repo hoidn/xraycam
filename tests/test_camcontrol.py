2662b88db6730812ce05fb788a9d81861d71d498
Oliver Hoidn <ohoidn@gmail.com>
2016-05-09 14:06:56 -0400

    Corrected behavior for new DataRun instances when data is absent on local machine.

---
+++ tests/test_camcontrol.py
@@ -0,0 +1,18 @@
+from xraycam import camcontrol
+import time
+
+    
+#def test_data_transfer():
+#    def get_datarun(run_prefix, run = False):
+#        return camcontrol.DataRun(run_prefix = run_prefix, run = run, gain = '0x3f')
+#    import os
+#    import time
+#    prefix = 'data/' + str(time.time())
+#    # Should run exposure series on BBB
+#    get_datarun(prefix, run = True)
+#    os.system('rm -r data')
+#    # Should copy data from BBB
+#    get_datarun(prefix, run = False)
+#    # Should load existing data
+#    get_datarun(prefix, run = False)
+
---
+++ venv/lib/python3.5/_weakrefset.py
@@ -0,0 +1,1 @@
+/home/oliver/anaconda3/lib/python3.5/_weakrefset.py
