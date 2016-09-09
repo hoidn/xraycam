ff5c55cb14fe69ec0881ff4cd564f4d6cafa2325
labxray <labxray@D-10-155-95-38.dhcp4.washington.edu>
2016-09-09 18:51:20 -0400

    compile libclusters.so in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+import os
+
+os.chdir('lib')
+os.system('make CFLAGS=-O3')
+os.chdir('../')
 
 setup(name = 'xraycam',
     packages = find_packages('.'),
@@ -7,14 +12,14 @@ setup(name = 'xraycam',
 #    scripts = [
 #        'bin/oacapture'
 #        ],
-    install_requires = ['paramiko', 'numpy', 'matplotlib', 'mpld3', 'plotly', 'humanfriendly'],
+    install_requires = ['paramiko', 'numpy', 'matplotlib', 'mpld3', 'plotly', 'humanfriendly', 'multiprocess'],
     zip_safe = False)
 
 # TODO: the below config isn't getting loaded (instead python grabs the package version). Fix this.
-import os
 import shutil
 configfile = os.path.expanduser('~/.xraycam/detconfig.py')
 configdir = os.path.dirname(configfile)
 if not os.path.exists(configdir):
     os.makedirs(configdir)
 shutil.copyfile('xraycam/detconfig.py', configfile)
+
