6d8ca22194498b373098dfd9779b424f83b827f3
wholden <holdenwm523@gmail.com>
2017-09-18 18:52:44 -0400

    updated nbinit so that all imports are taken care of at the start of a notebook

--- xraycam/nbinit.py
+++ xraycam/nbinit.py
@@ -2,4 +2,10 @@
 import matplotlib
 matplotlib.use('nbagg')
 import numpy as np
+import pandas as pd
+import xraycam
+from xraycam import camcontrol
+from xraycam import zwocapture
+from xraycam import camalysis
+from xraycam import scancontrol
 from xraycam.camcontrol import plt
